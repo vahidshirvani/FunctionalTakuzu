d0bbbebff75ef969baa10a35ad1667382760a311
Emanuel Stoeckli <emanuel.stoeckli@gmail.com>
2014-12-03 14:29:31 +0100

    Reads board of the game, converts and prints it

--- tic-tac-logic.hs
+++ tic-tac-logic.hs
@@ -4,27 +4,33 @@ charToIntList ('X':xs) list = charToIntList xs (1:list)
 charToIntList ('O':xs) list = charToIntList xs (0:list)
 charToIntList ('.':xs) list = charToIntList xs (-1:list)
 
-readLines :: Char -> Int -> [[Int]] -> IO ()
-readLines t 0 board = 
-    print (show (reverse board))
-readLines t x board = do
-    row <- getLine -- X
-    readLines t (x-1) ((charToIntList row []):board)
+printError :: String -> IO [[Int]]
+printError msg = do
+    putStrLn msg
+    return [[]]
 
-gameType :: [Char] -> IO ()
+readLines :: Char -> Int -> Int -> [[Int]] -> IO [[Int]]
+readLines gameChar _ 0 board = return (reverse board)
+readLines gameChar gameSize rowsLeft board = do
+    row <- getLine -- X
+    let rowList = (charToIntList row [])
+    if (length rowList) == gameSize
+        then readLines gameChar gameSize (rowsLeft-1) (rowList:board)
+        else printError "Invalid row length"
+
+gameType :: [Char] -> IO [[Int]]
 gameType ('T':xs) = do
     let pairs = (map (\x -> read x :: Int) (words xs))
     if ((pairs !! 0) == (pairs !! 1)) && ((mod (pairs !! 0) 2) == 0) 
-        then (readLines 'T' (pairs !! 0) [])
-        else print "incorrect input values"
-gameType ('S':xs) = 
-    print "sudoku"
-gameType _ = 
-    print "unknown character" 
+        then (readLines 'T' (pairs !! 0) (pairs !! 0) [])
+        else printError "incorrect input values"
+
+gameType ('S':xs) = printError "sudoku"
+gameType _ = printError "unknown character"
 
 main :: IO ()
 main = do
     gameInfo <- getLine
-    gameType gameInfo
-    
+    let board = gameType gameInfo
+    board >>= print -- this operator requires us to return IO
      
