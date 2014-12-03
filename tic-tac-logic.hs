642f79e24ed5040bfbe75bf52768d5079886ed62
Emanuel Stoeckli <emanuel.stoeckli@gmail.com>
2014-12-03 15:44:03 +0100

    New type for the game board

--- tic-tac-logic.hs
+++ tic-tac-logic.hs
@@ -1,17 +1,30 @@
 import Data.List (transpose)
 
+type Board = [[Int]]
+
+------------------------------------------------------------------------
+-- Algorithms from conceptispuzzles
+------------------------------------------------------------------------
+
+avoidTriplesOne :: Board -> Board
+avoidTriplesOne board = board
+
+------------------------------------------------------------------------
+-- Reading input
+------------------------------------------------------------------------
+
 charToIntList :: [Char] -> [Int] -> [Int]
 charToIntList [] list = (reverse list)
 charToIntList ('X':xs) list = charToIntList xs (1:list)
 charToIntList ('O':xs) list = charToIntList xs (0:list)
 charToIntList ('.':xs) list = charToIntList xs (-1:list)
 
-printError :: String -> IO [[Int]]
+printError :: String -> IO Board
 printError msg = do
     putStrLn msg
     return [[]]
 
-readLines :: Char -> Int -> Int -> [[Int]] -> IO [[Int]]
+readLines :: Char -> Int -> Int -> Board -> IO Board
 readLines gameChar _ 0 board = return (reverse board)
 readLines gameChar gameSize rowsLeft board = do
     row <- getLine -- X
@@ -20,7 +33,7 @@ readLines gameChar gameSize rowsLeft board = do
         then readLines gameChar gameSize (rowsLeft-1) (rowList:board)
         else printError "Invalid row length"
 
-gameType :: [Char] -> IO [[Int]]
+gameType :: [Char] -> IO Board
 gameType ('T':xs) = do
     let pairs = (map (\x -> read x :: Int) (words xs))
     if ((pairs !! 0) == (pairs !! 1)) && ((mod (pairs !! 0) 2) == 0) 
@@ -29,6 +42,10 @@ gameType ('T':xs) = do
 
 gameType ('S':xs) = printError "sudoku"
 gameType _ = printError "unknown character"
+
+------------------------------------------------------------------------
+-- Main
+------------------------------------------------------------------------
 
 main :: IO ()
 main = do
